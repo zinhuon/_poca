d056a8893ae49f2760de4422063fafa859f9c6b6
k.kou <zin.huon@gmail.com>
2024-06-14 18:50:01 +0900

    first commit

---
+++ resources/views/simple.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Page</title>
+</head>
+<body>
+    <h1>Simple Page</h1>
+    <p>This is a simple page.</p>
+    <p>title</p>
+</body>
+</html>
